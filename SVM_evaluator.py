474cf4f005b39b43ecd679318911e71a89fd2f1a
lynn <yelintongji@gmail.com>
2023-11-05 18:37:02 +0100

    added evaluator for SVM models

--- SVM_evaluator.py
+++ SVM_evaluator.py
@@ -3,7 +3,7 @@ import pickle
 import fasttext
 from dataloader import SentimentDataLoader
 from classifier_fasttext import TextClassifierFastText
-from classifier import identity
+from SVM_classifier import identity
 
 
 class Evaluator:
@@ -38,44 +38,31 @@ class Evaluator:
 
         # calculate various evaluation metrics
         accuracy = accuracy_score(self.test_labels, predictions)
-        precision = precision_score(self.test_labels, predictions, average='weighted')
-        recall = recall_score(self.test_labels, predictions, average='weighted')
-        f1 = f1_score(self.test_labels, predictions, average='weighted')
+        precision = precision_score(self.test_labels, predictions, average='macro')
+        recall = recall_score(self.test_labels, predictions, average='macro')
+        f1 = f1_score(self.test_labels, predictions, average='macro')
         conf_matrix = confusion_matrix(self.test_labels, predictions)
 
         print("Overall Metrics:")
         print("Accuracy:", accuracy)
-        print("Precision (weighted):", precision)
-        print("Recall (weighted):", recall)
-        print("F1 Score (weighted):", f1)
+        print("Precision:", precision)
+        print("Recall:", recall)
+        print("F1 Score:", f1)
         print("Confusion Matrix:")
         print(conf_matrix)
-
-        # calculate and print metrics for individual classes (if applicable)
-        class_precision = precision_score(self.test_labels, predictions, average=None)
-        class_recall = recall_score(self.test_labels, predictions, average=None)
-        class_f1 = f1_score(self.test_labels, predictions, average=None)
-
-        num_classes = len(class_precision)
-        for i in range(num_classes):
-            print(f"Metrics for Class {i}:")
-            print("Precision:", class_precision[i])
-            print("Recall:", class_recall[i])
-            print("F1 Score:", class_f1[i])
-       
+  
 
 if __name__ == "__main__":
     # create an Evaluator instance
     evaluator = Evaluator(evaluate_fasttext_classifier=False)
 
     # load the saved model
-    evaluator.load_classifier('TF-IDF_word_Linear SVM.pkl')  
+    evaluator.load_classifier('TF-IDF_char_Linear SVM.pkl')  
 
     # load test data
-    test_loader = SentimentDataLoader('dev_clean.tsv')
+    test_loader = SentimentDataLoader('test.tsv')
     test_loader.load_data()  
-    test_string_documents, test_tokenized_documents, test_labels, sentiment_labels, sentiment_scores = test_loader.get_data()
-    #print(test_tokenized_documents)
+    test_string_documents, test_tokenized_documents, test_labels, labels_bin, sentiment_labels, sentiment_scores = test_loader.get_data()
 
     evaluator.set_test_data(test_string_documents, test_labels)
 
