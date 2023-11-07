8f9f861821c8553fb5149d4865836bb1e1ede217
lynn <yelintongji@gmail.com>
2023-11-07 13:44:00 +0100

    added script for lstm

--- lstm.py
+++ lstm.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python
-
-
-"""Module for training and evaluating the LSTM model.
-
-Hyperparameters are to be specified in the code. Optimal values already set.
-The code also produces a confusion matrix.
-
-Example usage:
->>> python3 lstm.py -i train.txt -d dev.txt -t test.txt
-"""
-
-
 import json
 import argparse
 import pickle
@@ -19,72 +6,50 @@ import random as py_random
 import numpy as np
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM, Bidirectional, Dropout, Activation
+from keras.layers import Dense, Embedding, LSTM, Dropout, Bidirectional
 from keras.initializers import Constant
-from sklearn.metrics import accuracy_score, recall_score, precision_score, precision_recall_curve, f1_score, confusion_matrix
+from sklearn.metrics import f1_score, confusion_matrix
 from sklearn.preprocessing import LabelBinarizer
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+from dataloader import SentimentDataLoader
 
 
-# Random seed for reproducibility
 np.random.seed(1234)
 tf.random.set_seed(1234)
-py_random.seed(1234)
+tf.config.threading.set_intra_op_parallelism_threads(1)
+tf.config.threading.set_inter_op_parallelism_threads(1)
+py_random = np.random.RandomState(1234)
 
-
-def create_arg_parser() -> argparse.Namespace:
-    """Converts command line arguments to Namespace object and returns it."""
+def create_arg_parser():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-i", "--train_file", default="data/train_clean.tsv", type=str,
-                        help="Input file to learn from (default train_clean.txt)")
-    parser.add_argument("-d", "--dev_file", type=str, default="data/dev_clean.tsv",
+    parser.add_argument("-i", "--train_file", default="train.tsv", type=str,
+                        help="Input file to learn from (default train.txt)")
+    parser.add_argument("-d", "--dev_file", type=str, default="dev.tsv",
                         help="Separate dev set to read in (default dev.txt)")
     parser.add_argument("-t", "--test_file", type=str,
                         help="If added, use trained model to predict on test set")
-    parser.add_argument("-e", "--embeddings", default="glove_reviews.json", type=str,
-                        help="Embedding file we are using (default glove_reviews.json)")
+    parser.add_argument("-e", "--embeddings", default="glove_embeddings.json", type=str,
+                        help="Using GloVe embeddings")
 
     args = parser.parse_args()
 
     return args
 
-
-def read_corpus(corpus_file: str) -> tuple[list[str], list[str]]:
-    """Reads review data set and returns docs and labels."""
-
-    documents, labels = [], []
-
-    with open(corpus_file, encoding="utf-8") as f:
-        for line in f:
-            tokens = line.split()
-
-            label = tokens[-1]
-            tweet = ' '.join(tokens[:-1])
-            documents.append(tweet)
-            labels.append(label)
-
-    return documents, labels
-
-
-def read_embeddings(embeddings_file: str):
-    """Reads in word embeddings from file and save as numpy array."""
-
+def read_embeddings(embeddings_file):
+    # read the filtered pretrained embeddings    
     embeddings = json.load(open(embeddings_file, 'r'))
     return {word: np.array(embeddings[word]) for word in embeddings}
 
 
 def get_emb_matrix(voc, emb):
-    """Creates embedding matrix given vocab and the embeddings."""
-
+    # old function from assignment 3
     num_tokens = len(voc) + 2
     word_index = dict(zip(voc, range(len(voc))))
-    # Bit hacky, get embedding dimension from the word "the"
     embedding_dim = len(emb["the"])
-    # Prepare embedding matrix to the correct size
     embedding_matrix = np.zeros((num_tokens, embedding_dim))
     for word, i in word_index.items():
         embedding_vector = emb.get(word)
@@ -95,94 +60,55 @@ def get_emb_matrix(voc, emb):
     return embedding_matrix
 
 
-def create_model(Y_train: list[str], emb_matrix) -> Sequential:
-    """Creates the bidirectional LSTM model."""
-
-    # Hyperparameters
-    HIDDEN_UNITS = 64
-    LEARNING_RATE = 0.01
-    LOSS_FUNCTION = "binary_crossentropy"
-    OPTIM = Adam(learning_rate=LEARNING_RATE)
-
-    # Take embedding dim and size from emb_matrix
-    embedding_dim = len(emb_matrix[0])  # 300
-    print(f"Embedding dim: {embedding_dim}")  # For debugging
-
+def create_model(emb_matrix, hidden_units, loss_function, optim, recurrent_dropout, dropout):
+    # take embedding dim and size from emb_matrix
+    embedding_dim = len(emb_matrix[0]) 
     num_tokens = len(emb_matrix)
-    print(f"Num tokens: {num_tokens}")  # For debugging
 
-    num_labels = len(set(Y_train))
-    print(f"Num labels: {num_labels}")  # For debugging
-
+    # model architecture
     model = Sequential()
     model.add(
         Embedding(num_tokens, embedding_dim, embeddings_initializer=Constant(emb_matrix), trainable=False)
     )
 
     model.add(
-        LSTM(units=HIDDEN_UNITS, return_sequences=False, recurrent_dropout=0.5)
+        LSTM(units=hidden_units, return_sequences=False, recurrent_dropout=recurrent_dropout)
     )
-
-    model.add(Dropout(0.8))
+        
+    model.add(Dropout(dropout))
 
     model.add(Dense(input_dim=10, units=1, activation='sigmoid'))
 
-
-    # TODO: Implement with different loss metric
-    model.compile(loss=LOSS_FUNCTION, optimizer=OPTIM, metrics=[tf.keras.metrics.Recall(
+    model.compile(loss=loss_function, optimizer=optim, metrics=[tf.keras.metrics.Recall(
     thresholds=None, top_k=None, class_id=None, name=None, dtype=None
 )])
 
     return model
 
 
-def train_model(model: Sequential,
-                X_train: list[str], Y_train: list[str],
-                X_dev: list[str], Y_dev: list[str]) -> Sequential:
-    """Trains the LSTM model."""
-
-    VERBOSE = 1
-    BATCH_SIZE = 25
-    EPOCHS = 20
-
+def train_model(model, X_train, Y_train, X_dev, Y_dev, verbose, batch_size, epochs):
+    
     callback = EarlyStopping(monitor="val_loss", patience=3)
     model.fit(
         X_train,
         Y_train,
-        verbose=VERBOSE,
-        epochs=EPOCHS,
+        verbose=verbose,
+        epochs=epochs,
         callbacks=[callback],
-        batch_size=BATCH_SIZE,
+        batch_size=batch_size,
         validation_data=(X_dev, Y_dev)
     )
     test_set_predict(model, X_dev, Y_dev, "dev")
 
     return model
 
-def custom_argmax(value,threshold=0.5):
-    if value >= threshold:
-        return 1
-    else:
-        return 0
+def test_set_predict(model, X_test, Y_test, ident):
 
+    # if >= 0.5 return 1, else 0
+    Y_pred = list(map(lambda value: 1 if value >= 0.5 else 0, model.predict(X_test)))
 
-def test_set_predict(model: Sequential, X_test: list[str], Y_test: list[str], ident: str) -> None:
-    """Measure accuracy on own test set, which is a subset of the training data."""
-
-    Y_pred = [custom_argmax(value) for value in model.predict(X_test)]
-
-
-    formatted_acc = accuracy_score(Y_test, Y_pred)
-    print(f"Accuracy on own {ident} set: {formatted_acc}")
-
-    formatted_prec = round(precision_score(Y_test, Y_pred, average='macro'), 3)
-    print(f"Precision on own {ident} set: {formatted_prec}")
-
-    formatted_rec = round(recall_score(Y_test, Y_pred, average='macro'), 3)
-    print(f"Recall on own {ident} set: {formatted_rec}")
-
-    formatted_f1 = round(f1_score(Y_test, Y_pred, average='macro'), 3)
-    print(f"F1 on own {ident} set: {formatted_f1}")
+    f1_result = round(f1_score(Y_test, Y_pred, average='macro'), 3)
+    print(f"F1 on {ident} set: {f1_result}")
 
     if ident == "test":
         conf_matrix = confusion_matrix(Y_test, Y_pred)
@@ -191,51 +117,84 @@ def test_set_predict(model: Sequential, X_test: list[str], Y_test: list[str], id
         with open('confusion_matrix.pkl', 'wb') as file:
             pickle.dump(conf_matrix, file)
 
-
-def main() -> None:
-    """Main function to train and test neural network given cmd line arguments."""
-
+def main():
+    
     args = create_arg_parser()
 
-    # Read in the data and embeddings
-    X_train, Y_train = read_corpus(args.train_file)
-    X_dev, Y_dev = read_corpus(args.dev_file)
+    # set the hyperparameters for creating a model
+    hidden_units = 64
+    learning_rate = 0.005
+    loss_function = 'binary_crossentropy'
+    optim = Adam(learning_rate=learning_rate)
+    recurrment_dropout = 0.6
+    dropout = 0.1
+    # set the hyperparameters for training the model
+    verbose = 1
+    batch_size = 16
+    epochs = 20
+
+    # read in the training data using dataloader
+    train_loader = SentimentDataLoader(args.train_file)
+    train_loader.load_data()
+    X_train, _, Y_train, _, _, _ = train_loader.get_data()
+
+    # read in the dev data using dataloader
+    dev_loader = SentimentDataLoader(args.dev_file)
+    dev_loader.load_data()
+    X_dev, _, Y_dev, _, _, _ = dev_loader.get_data()
 
     embeddings = read_embeddings(args.embeddings)
 
-    # Transform words to indices using a vectorizer
+    # transform words to indices using a vectorizer
     vectorizer = TextVectorization(standardize=None, output_sequence_length=50)
-    # Use train and dev to create vocab - could also do just train
+    # create vocab 
     text_ds = tf.data.Dataset.from_tensor_slices(X_train + X_dev)
     vectorizer.adapt(text_ds)
-    # Dictionary mapping words to idx
+
+    # dictionary mapping words to idx
     voc = vectorizer.get_vocabulary()
     emb_matrix = get_emb_matrix(voc, embeddings)
 
-    # Transform string labels to one-hot encodings
+    # transform string labels to one-hot encodings
     encoder = LabelBinarizer()
-    Y_train_bin = encoder.fit_transform(Y_train)  # Use encoder.classes_ to find mapping back
+    Y_train_bin = encoder.fit_transform(Y_train) 
     Y_dev_bin = encoder.fit_transform(Y_dev)
 
-    # Create model
-    model = create_model(Y_train, emb_matrix)
+    # create model
+    model = create_model(emb_matrix, hidden_units=hidden_units, loss_function=loss_function, optim=optim, recurrent_dropout=recurrment_dropout, dropout=dropout)
 
-    # Transform input to vectorized input
+    # transform input to vectorized input
     X_train_vect = vectorizer(np.array([[s] for s in X_train])).numpy()
     X_dev_vect = vectorizer(np.array([[s] for s in X_dev])).numpy()
 
-    # Train the model
-    model = train_model(model, X_train_vect, Y_train_bin, X_dev_vect, Y_dev_bin)
+    # train the model
+    model = train_model(model, X_train_vect, Y_train_bin, X_dev_vect, Y_dev_bin, verbose=verbose, batch_size=batch_size, epochs=epochs)
 
-    # Do predictions on specified test set
+    # determine the model name based on the argument
+    if "fasttext" in args.embeddings:
+        model_name = 'fasttext'
+    elif "glove" in args.embeddings:
+        model_name = 'glove'
+    else:
+        model_name = 'vanilla'
+
+    # save the trained model with the appropriate name
+    saved_model_name = f'{model_name}_model.h5'
+    model.save(f'{model_name}_model.h5')
+    print(f"Model saved as: {saved_model_name}")
+
+    # do predictions on specified test set
     if args.test_file:
-        # Read in test set and vectorize
-        X_test, Y_test = read_corpus(args.test_file)
-        Y_test_bin = encoder.fit_transform(Y_test)
-        X_test_vect = vectorizer(np.array([[s] for s in X_test])).numpy()
-        # Finally do the predictions
-        test_set_predict(model, X_test_vect, Y_test_bin, "test")
 
+        # read in the training data using dataloader
+        test_loader = SentimentDataLoader(args.test_file)
+        test_loader.load_data()
+        X_test, _, Y_test, _, _, _ = test_loader.get_data()
+
+        Y_test_bin = encoder.transform(Y_test)
+        X_test_vect = vectorizer(np.array([[s] for s in X_test])).numpy()
+        
+        test_set_predict(model, X_test_vect, Y_test_bin, "test")
 
 if __name__ == "__main__":
     main()
